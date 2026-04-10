acdefc013d02d10844642bc44320ad79af7f55ac
Chen-daydayup <mydatingbrian@mail.dlut.edu.cn>
2026-04-10 13:41:34 +0800

    Import all functions from metrics, visualization, and table_generator

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .metrics import dice_score, iou_score
-from .visualization import visualize_result
-from .table_generator import save_comparison_table
+from .metrics import *
+from .table_generator import *
+from .visualization import *
